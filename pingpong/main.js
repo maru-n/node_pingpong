533bae2ab608410e16d41b218bfadce6b0bcc7e7
bob_1983 <bob.1983.g@gmail.com>
2013-06-13 22:51:36 +0900

    コミット

--- pingpong/main.js
+++ pingpong/main.js
@@ -1,3 +1,11 @@
+    //これはテストです。gingbear
+    //reconnect:falseは、切断した時に再接続しないため。
+    //これを入れないと、サーバが落ちた時にエラーでる。
+    var socket = io.connect('http://'+location.hostname, {
+      'reconnect': false
+    });
+
+
 function init(){
 
     const STAGE_WIDTH = 640;
@@ -39,6 +47,26 @@ function init(){
         rect.setPosition(x, rect.getPosition().y);
         layer.draw();
 
+        var absPosX = x / field.getWidth();
+        var msg = {cursolX: absPosX};
+
+        // サーバにXの位置を送信
+        socket.send(msg);
+    });
+
+    //socket.send()で送信されたメッセージは'message'のハンドラで取得できる。
+    socket.on('message', function (data) {
+        var absPosX = data.getBallX;
+        var absPosY = data.getBallY;
+        var x = absPosX * field.getWidth() - rect.getWidth()/2;
+        // var y = absPosY * field.getHeight() - rect.getHeight()/2;
+        rect.setPosition(x, rect.getPosition().y);
+        layer.draw();
+    });
+
+    //切断されたときのハンドラ
+    socket.on('disconnect', function(message){
+      $("#message-area").append('切断されました');
     });
 }
 
