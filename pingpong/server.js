5e13b19207984e9edf55d1910bcfc82e56ec2497
t-kanda <t-kanda@phybbit.com>
2013-06-13 22:58:24 +0900

    ボールの位置を返す機能を追加

--- pingpong/server.js
+++ pingpong/server.js
@@ -10,17 +10,23 @@ function handler (req, res) {
             res.writeHead(500);
             return res.end('Error loading index.html');
         }
-        
         res.writeHead(200);
         res.end(data);
     });
 };
 
+io.sockets.num=0;
 io.sockets.on('connection', function (socket) {
+    if(++io.sockets.num>2)
+        //TODO:複数プレイは後で対応
+        return;
 
+    var t=1;
     setInterval(function() {
-        socket.send("[server] >> おーーーーい。");
-    }, 5000);
+        var theta=++t/180*Math.PI; 
+        console.log(theta);
+        socket.send({ballX:Math.cos(theta),ballY:Math.sin(theta)});
+    }, 100);
 
 
     socket.on('message', function (data) {
