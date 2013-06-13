d701bc57892a9260c1529199b38eb383c47f0227
maruyama <norihiro.maruyama@gmail.com>
2013-06-13 22:32:59 +0900

    start up

---
+++ pingpong/server.js
@@ -0,0 +1,31 @@
+var app = require('http').createServer(handler),
+    io = require('socket.io').listen(app),
+    fs = require('fs');
+
+app.listen(3001);
+
+function handler (req, res) {
+    fs.readFile(__dirname + '/index.html',function (err, data) {
+        if (err) {
+            res.writeHead(500);
+            return res.end('Error loading index.html');
+        }
+        
+        res.writeHead(200);
+        res.end(data);
+    });
+};
+
+io.sockets.on('connection', function (socket) {
+
+    setInterval(function() {
+        socket.send("[server] >> おーーーーい。");
+    }, 5000);
+
+
+    socket.on('message', function (data) {
+        var msg = "[" + socket.id + "] >> " + data;
+        socket.send(msg);
+        socket.broadcast.send(msg);
+    });
+});
