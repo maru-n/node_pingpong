527d082320d5894c713544e375b8cbd05f4f42b8
maruyama <norihiro.maruyama@gmail.com>
2013-06-23 00:03:09 +0900

    game.jsを修正

--- pingpong/game.js
+++ pingpong/game.js
@@ -1,3 +1,25 @@
-var Game = function() {
+exports.getNewGame = function() {
+    var args = arguments;
+    var game = new Game();
+    return game;
 }
 
+var Game = function() {
+    this.playerNum = 0;
+}
+
+Game.prototype = {
+    setSocket : function(socket) {
+    },
+
+    getPlayerNum : function() {
+        return this.playerNum;
+    }
+};
+
+function test() {
+    var game = exports.getNewGame();
+    console.log(game.getPlayerNum());
+}
+
+//test();
